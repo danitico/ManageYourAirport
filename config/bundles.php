d461c68410486c88f60e94f70be11a583739c705
Daniel Ranchal <danitico98@gmail.com>
2018-04-19 17:14:13 +0200

    Changed some things

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,7 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Harmony\Bundle\UiBundle\HarmonyUiBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
