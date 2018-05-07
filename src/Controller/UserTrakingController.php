1e4b4a4d426a1ad40029e22a9eed7f97c0b24c2c
Diego Rodríguez Riera <diegorodriguezriera@gmail.com>
2018-05-07 21:01:36 +0200

    added location class and improved make steps towards getting the user location without a form

--- src/Controller/UserTrakingController.php
+++ src/Controller/UserTrakingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Form\LocationType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,8 +13,18 @@ class UserTrakingController extends Controller
     /**
      * @Route("/track", name="user_traking")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $location=new Location();
+        $form=$this->createForm(LocationType::class, $location);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            $entitiManager=$this->getDoctrine()->getManager();
+            $entitiManager->persist($location);
+            $entitiManager->flush();
+        }
+
         return $this->render('user_traking/index.html.twig', [
             'controller_name' => 'UserTrakingController',
         ]);
