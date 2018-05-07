cbfa98590e4b535d256666047f82e4c424ee0392
Daniel Ranchal <danitico98@gmail.com>
2018-05-07 12:04:46 +0200

    Cuando el usuario se logea de manera correcta, se redirige al path userpage

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,10 +25,14 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+
+
         return $this->render('security/login.html.twig', array(
             'last_username' => $lastUsername,
-            'error'         => $error,
+            'error' => $error,
         ));
+
+
     }
 
     /**
@@ -62,7 +66,7 @@ class SecurityController extends Controller
 
             $user->eraseCredentials();
 
-            return $this->redirectToRoute('homepage');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('security/register.html.twig', [
