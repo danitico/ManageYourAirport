e7807e4c807a13b47f4415c2c1f7272eea4a4051
Diego Rodríguez Riera <diegorodriguezriera@gmail.com>
2018-05-07 15:13:47 +0200

    added user coordinates

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,6 +25,8 @@ class SecurityController extends Controller
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+
+        }
         return $this->render('security/login.html.twig', array(
             'last_username' => $lastUsername,
             'error'         => $error,
