dd142137f8eea7d0b0327515d442e8d4ff150b99
Diego Rodríguez Riera <diegorodriguezriera@gmail.com>
2018-05-08 20:15:44 +0200

    refactorize the twigs

--- src/Controller/UserTrackingController.php
+++ src/Controller/UserTrackingController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Location;
+use App\Form\LocationType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,8 +13,19 @@ class UserTrackingController extends Controller
     /**
      * @Route("/track", name="user_tracking")
      */
-    public function index()
+    public function index(Request $request)
     {
+        $location=new Location();
+        $form = $this->createForm(LocationType::class, $location);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($location);
+            $manager->flush();
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('user_tracking/index.html.twig', [
             'controller_name' => 'UserTrackingController',
         ]);
